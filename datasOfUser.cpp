bd3c2d01e2485283da6d552389e3d58f775b6db5
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-05 16:17:44 -0300

    🚧 feat: implemented a function for receive and print datas of the user

---
+++ datasOfUser.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+
+    int age;
+    char sex;
+    float height;
+
+    cout << "Insert your age:  ";
+    cin>> age;
+
+    cout << "Insert your sex:  ";
+    cin>>sex;
+
+    cout << "Insert your height:  ";
+    cin>>height;
+
+    cout << "Age: "<< age << endl;
+    cout << "Sex: "<< sex << endl;
+    cout << "Height: "<< height << endl;
+
+
+    return 0;
+}
---
+++ priceImposteProduct.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    float price, iva, finalPrice;
+
+    cout << "Insert the price of the product:  "; cin>>price;
+
+    iva = price * 0.21;
+
+
+    finalPrice = price + iva;
+
+    cout << "The price of the product more the imposte is:  " << finalPrice << endl;
+
+
+
+    return 0;
+}
