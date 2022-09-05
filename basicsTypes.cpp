2ca3a46fc7fb5347cad1881d3969929dd3591bec
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-05 15:00:11 -0300

    🎉 Start repository!

---
+++ basicsTypes.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+
+    int number = 10;
+
+    float floatingNumber = 10.67;
+
+    char letter = 'A';
+
+    cout <<number<<floatingNumber<<letter<<endl;
+
+    return 0;
+}
---
+++ entryOrSaid.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int number;
+
+    cout << "Insert a number:  ";
+    cin >> number;
+
+    cout << "The number inserted is:  " << number;
+
+    return 0;
+}
