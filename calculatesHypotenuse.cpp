8f1e3abb7a50079e5beb5508f5bf6b6820aca785
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-06 17:35:47 -0300

    ♻️ feat: exercise of the calculate hypotenuse of the triangle retangle refactored.

--- calculatesHypotenuse.cpp
+++ calculatesHypotenuse.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <math.h>
 
 using namespace std;
 
@@ -13,17 +14,12 @@ int main () {
     cout << "User, insert the value the of primary peccarie: ";
     cin>>peccarie1;
 
-
     cout << "User, insert the value the of second peccarie: ";
     cin>>peccarie2;
 
-
-    hipotenuse = (peccarie1 * 2) + (peccarie2 * 2);
-
+    hipotenuse = sqrt(pow(peccarie1, 2) + pow(peccarie2, 2));
 
     cout << "The Hipotenuse of the triangle retangle is:  " << hipotenuse;
-
-
 
     return 0;
 }
