ff7359236501fa24748c81797697f2136425a41f
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-09 09:49:30 -0300

    🚧 feat: exercise for verify if the number is positive or negative

---
+++ positiveOrNegative.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main () {
+
+    int number;
+
+
+    cout << "Insert a number:  ";
+    cin>>number;
+
+    number < 0 ? cout << "It is negative." << endl : cout << "it is positive" << endl;
+
+
+    return 0;
+}
