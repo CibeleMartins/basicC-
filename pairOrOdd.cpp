8e79f0fe80f4b5ef0295f5242791e34bbe340bab
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-09 09:42:34 -0300

    🚧 feat: exercise for verify if the number inserted is pair or odd

---
+++ pairOrOdd.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main () {
+
+
+    int number;
+
+    cout << "Insert a number:  ";
+    cin>>number;
+
+    switch (number % 2)
+    {
+    case 0: cout << "Number is pair";
+        break;
+    
+    default: cout << "Number is odd";
+        break;
+    }
+
+
+    return 0;
+}
