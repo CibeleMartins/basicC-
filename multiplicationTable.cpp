b54753abcf75f7d00f38042af93885501c5e2fb7
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-12 14:48:30 -0300

    🚧 feat: multiplication table with loop for

---
+++ multiplicationTable.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main () {
+
+    int number;
+
+    cout << "User, insert a number of the 1 to 10, please: ";
+    cin>>number;
+
+    if (number <= 10) {
+
+   
+        cout << "Multiplication Table" << endl;
+
+        for (int i = 0; i < 10; i++) {
+
+            cout << number << " x " << i + 1 << endl;
+        }
+
+    } else {
+
+        cout << "Plese, insert a number between 1 and 10." << endl;
+
+        cin>>number;
+
+        cout << "Multiplication Table" << endl;
+
+        for (int i = 0; i < 10; i++) {
+
+            cout << number << " x " << i + 1 << endl;
+        }
+
+    }
+
+
+
+    return 0;
+}
