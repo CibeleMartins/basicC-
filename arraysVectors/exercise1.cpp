16141f4b08e2d735791cbb95693accfb19731f96
Cibele Martins <Cibele.Martins@ibm.com>
2022-09-15 11:47:47 -0300

    🚧 feat: Implement exercise that sum values in array

---
+++ arraysVectors/exercise1.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int numbers [11] = {1,2,3,4,5,6,7,8,9,10};
+
+    int sum = 0;
+
+    for (int i =0; i < sizeof numbers/ sizeof numbers[0]; i++) {
+
+        sum += i;
+
+
+    }
+
+    cout << "Sum is: " << sum;
+
+
+    return 0;
+}
